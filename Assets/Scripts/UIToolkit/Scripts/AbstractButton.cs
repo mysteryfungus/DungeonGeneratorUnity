b0ea9d047033afffcde567320f3240af0c25e9a9
dor1mae <dorima.bop@gmail.com>
2024-09-25 21:35:44 +0700

    Частичный UI
    Совершен частичный перенос на UIToolkit: реализована система вкладок с разным уровнем сложности настройки, введена система инициализации скриптов на визуальных элементах, добавлены эффекты наведения и нажатия для кнопок интерфейса.
    
    Реализован кастомный класс ввода текстовой информации с возможность базовой кастомизации в зависимости от нужд при работе в UIBuilder.
    
    На данный момент новый вид интерфейса находится в отдельной сцене

---
+++ Assets/Scripts/UIToolkit/Scripts/AbstractButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public abstract class AbstractButton : MonoBehaviour, ILink
+{
+    [SerializeField] protected string nameOfElement;
+    protected VisualElement root;
+
+    public void Link(UIDocument uIDocument)
+    {
+        root = uIDocument.rootVisualElement;
+
+        Button button = root.Q<Button>(nameOfElement);
+
+        if (button != null)
+        {
+            Debug.Log($"{nameOfElement} был привязан");
+            button.clicked += Action;
+        }
+    }
+
+    public abstract void Action();
+}
