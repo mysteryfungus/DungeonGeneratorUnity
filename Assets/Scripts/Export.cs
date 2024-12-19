45831787ec77b414b00a5cec188d8cf8cbd8f795
wound3d <emelya2003@mail.ru>
2024-12-19 10:24:59 +0700

    Новая функция для работы с Toolkit
    Добавлена новая функция, в которой должна быть реализована та же логика, что и раньше, но с UI Toolkit

--- Assets/Scripts/Export.cs
+++ Assets/Scripts/Export.cs
@@ -15,12 +15,13 @@ public class Export : MonoBehaviour
 
     public void OnClick()
     {
-        ui.enabled = false;
-        StartCoroutine(CaptureAndSaveAsPDF());
+        StartCoroutine(NoToolkitCaptureAndSaveAsPDF()); // старая функция без Toolkit'а
+        // StartCoroutine(ToolkitCaptureAndSaveAsPDF()); — новая с Toolkit'ом
     }
 
-    IEnumerator CaptureAndSaveAsPDF()
+    IEnumerator NoToolkitCaptureAndSaveAsPDF()
     {
+        ui.enabled = false;
         cam.orthographicSize = 26;
         string screenPath = "tilemap.png";
         Debug.Log("Trying to make a screenshot...\n");
@@ -42,5 +43,34 @@ public class Export : MonoBehaviour
             ui.enabled = true;
         }
         else ui.enabled = true; 
+    }
+
+    // эту функция делает то же самое, что и функция выше, единственное логика скрытия и показа UI должна быть подстроена под UI-Toolkit
+    // как только здесь появится логика работы с UI, удалить старую функцию
+    // UI скрывается, чтобы сделать нормальный скриншот карты
+    public IEnumerator ToolkitCaptureAndSaveAsPDF(){
+        // ui.enabled = false;
+        cam.orthographicSize = 26;
+        string screenPath = "tilemap.png";
+        Debug.Log("Trying to make a screenshot...\n");
+        ScreenCapture.CaptureScreenshot(screenPath);
+        yield return new WaitForSeconds(1f);
+
+        string path = EditorUtility.SaveFilePanel("Экспорт в PDF", "", "Dungeon", "pdf");
+        if (!string.IsNullOrEmpty(path))
+        {
+            PdfDocument doc = new PdfDocument();
+            PdfPage page = doc.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            using (XImage img = XImage.FromFile(screenPath))
+            {
+                page.Width = img.PixelWidth; page.Height = img.PixelHeight;
+                gfx.DrawImage(img, 0, 0, page.Width, page.Height);
+                doc.Save(path);
+            }
+            // ui.enabled = true;
+        }
+        // else ui.enabled = true; 
+
     }
 }
